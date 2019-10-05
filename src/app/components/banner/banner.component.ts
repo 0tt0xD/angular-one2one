760792d60d8aa0cb8af28a0274c00bb5530594b5
Fabian <Fabian@users-Air.fritz.box>
2019-10-05 12:25:07 +0200

    add icons

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
 
-  constructor() { }
+  notMain : boolean = true;
+
+  constructor(
+    private router : Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  showMain() {
+    this.router.navigate(['/'])
   }
 
 }
